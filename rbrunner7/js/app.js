1e1d882490ddd574775d20588623198290b148e1
plowsof <77655812+plowsof@users.noreply.github.com>
2021-09-06 16:30:32 +0100

    Add files via upload

--- rbrunner7/js/app.js
+++ rbrunner7/js/app.js
@@ -44,4 +44,5 @@ async function CopyToClipboard(id)
 }
 //on page load - render the wishlist. set a 'time updated variable from the json' then loop compare
 //infinite loop
+renderWishlist()
 setInterval('renderWishlist()',1000)
